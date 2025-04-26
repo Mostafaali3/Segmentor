730b90b4012bee9c791d5b2ecfc7e56633c8af10
Mostafaali3 <mostafaalisarhan@gmail.com>
2025-04-27 02:58:11 +0300

    base code is done

---
+++ main.py
@@ -0,0 +1,62 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QFrame, QVBoxLayout, QSlider, QComboBox, QPushButton, \
+    QStackedWidget, QWidget, QFileDialog, QRadioButton, QDialog, QLineEdit, QHBoxLayout, QSpinBox
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt, QTimer
+from classes.image import Image
+from classes.image_viewer import ImageViewer
+from enums.viewerType import ViewerType
+from classes.controller import Controller
+import cv2
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi('main.ui', self)
+        
+        self.input_viewer_layout = self.findChild(QVBoxLayout,'input_layout')
+        self.input_viewer = ImageViewer()
+        self.input_viewer.viewer_type = ViewerType.INPUT
+        self.input_viewer_layout.addWidget(self.input_viewer)
+        
+        self.output_viewer_layout = self.findChild(QVBoxLayout,'output_layout')
+        self.output_viewer = ImageViewer()
+        self.output_viewer.viewer_type = ViewerType.OUTPUT
+        self.output_viewer_layout.addWidget(self.output_viewer)
+        
+        self.threshold_button = self.findChild(QPushButton, "thresholding_button")
+        self.threshold_button.clicked.connect(self.on_threshold_mode_button_pressed)
+        self.segmentation_button = self.findChild(QPushButton, "segmentation_button")
+        self.segmentation_button.clicked.connect(self.on_segmentation_mode_button_pressed)
+        self.modes_stacked_widget = self.findChild(QStackedWidget, "stackedWidget")
+        
+        self.controller = Controller(self.input_viewer, self.output_viewer)
+        
+        self.browse_button = self.findChild(QPushButton, "browse")
+        self.browse_button.clicked.connect(self.browse_)
+        
+        
+    def on_threshold_mode_button_pressed(self):
+        self.modes_stacked_widget.currentIndex(0)
+    def on_segmentation_mode_button_pressed(self):
+        self.modes_stacked_widget.currentIndex(1)
+        
+    def browse_(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Open Image File', '', 'Image Files (*.jpeg *.jpg *.png *.JPG);;All Files (*)')
+        if file_path:
+            if file_path.endswith('.jpeg') or file_path.endswith('.jpg') or file_path.endswith('.png') or file_path.endswith('.JPG'):
+                temp_image = cv2.imread(file_path)
+                image = Image(temp_image)
+                self.input_viewer.current_image = image
+                self.output_viewer.current_image = image
+                self.controller.update()
+        
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
